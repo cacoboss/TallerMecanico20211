c7ade8319a79a48ad1ec6b041b5cfefde4fd726f
Ciro Andres Cobos Sanchez <cacoboss@unal.edu.co>
2021-02-17 18:36:52 -0500

    Cargue del la capa de entidad, datos y formularios

---
+++ TallerMecanicoRepo/TallerMecanico.Datos/DAO_Categoria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TallerMecanico.Entidades;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TallerMecanico.Datos
+{
+    public class DAO_Categoria
+    {
+        string _cadenaConexion;
+
+        public string CadenaConexion
+        {
+            get
+            {
+                if(_cadenaConexion == null)
+                {
+                    _cadenaConexion =
+                        ConfigurationManager.ConnectionStrings["Conex"].ConnectionString;
+                }
+                return _cadenaConexion;
+            }
+            set => _cadenaConexion = value;
+        }
+
+
+    }
+}
---
+++ TallerMecanicoRepo/TallerMecanico.Entidades/Categoria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerMecanico.Entidades
+{
+    public class Categoria
+    {
+        int _id;
+        string _codigo;
+        string _nombre;
+        string _observacion;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Codigo { get => _codigo; set => _codigo = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Observacion { get => _observacion; set => _observacion = value; }
+
+        // ############# Constructores
+        public Categoria(int id, string codigo, string nombre, string observacion)
+        {
+            _id = id;
+            _codigo = codigo;
+            _nombre = nombre;
+            _observacion = observacion;
+        }
+        public Categoria(int id, string codigo, string nombre) : this (id, codigo, nombre, "")
+        {
+        }
+        public Categoria(int id, string nombre) : this (id, "", nombre, "")
+        {
+        }
+        public Categoria() : this (0, "", "", "")
+        {
+        }
+    }
+}
